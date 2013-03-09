34fe1f6657ffeefc55e9be5629e6477c478398f1
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-03-09 09:44:21 +0200

    Release 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='slt.portlet',
-    version='0.2',
+    version='0.3',
     description='Portlet for SLT site.',
     long_description=open("README.rst").read(),
     classifiers=[
